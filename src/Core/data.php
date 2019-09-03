36bd74d17685c6102fb2a86b88e56b09364c1204
Marina <marychizh@yandex.ru>
2019-09-03 20:25:14 +0300

    03/09

--- src/Core/data.php
+++ src/Core/data.php
@@ -4,4 +4,39 @@
     'nick' => 'zdfsgf',
     'message' => 'Enter your message',
   ),
+  1 => 
+  array (
+    'nick' => 'Марина',
+    'message' => 'Письмо',
+  ),
+  2 => 
+  array (
+    'nick' => 'Marina',
+    'message' => 'Enter your message',
+  ),
+  3 => 
+  array (
+    'nick' => 'Marina',
+    'message' => 'Enter your message',
+  ),
+  4 => 
+  array (
+    'nick' => 'Marina',
+    'message' => 'message',
+  ),
+  5 => 
+  array (
+    'nick' => 'Marina',
+    'message' => 'Enter your message',
+  ),
+  6 => 
+  array (
+    'nick' => 'Марина',
+    'message' => 'Enter your message',
+  ),
+  7 => 
+  array (
+    'nick' => ',jgkl',
+    'message' => 'Enter your message',
+  ),
 );
