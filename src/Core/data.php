76b6628d8db7507df66f803463bf384646c90156
Marina <marychizh@yandex.ru>
2019-09-03 20:53:11 +0300

    Добавление csv файла

--- src/Core/data.php
+++ src/Core/data.php
@@ -1,42 +1,7 @@
 <?php return array (
   0 => 
   array (
-    'nick' => 'zdfsgf',
-    'message' => 'Enter your message',
-  ),
-  1 => 
-  array (
-    'nick' => 'Марина',
-    'message' => 'Письмо',
-  ),
-  2 => 
-  array (
     'nick' => 'Marina',
-    'message' => 'Enter your message',
-  ),
-  3 => 
-  array (
-    'nick' => 'Marina',
-    'message' => 'Enter your message',
-  ),
-  4 => 
-  array (
-    'nick' => 'Marina',
-    'message' => 'message',
-  ),
-  5 => 
-  array (
-    'nick' => 'Marina',
-    'message' => 'Enter your message',
-  ),
-  6 => 
-  array (
-    'nick' => 'Марина',
-    'message' => 'Enter your message',
-  ),
-  7 => 
-  array (
-    'nick' => ',jgkl',
     'message' => 'Enter your message',
   ),
 );
