8cc6212145313c9f963c01108d51bf4aca01cfb9
Marina <marychizh@yandex.ru>
2019-09-19 18:15:34 +0300

    19/09/2019

--- templates/Auth/Form.php
+++ templates/Auth/Form.php
@@ -1,5 +1,13 @@
+<div class="formPosition">
 <form action="<?= $formPath ?>" method="post">
-<input type="text" name="login" placeholder="login"><br>
-<input type="password" name="password" placeholder="password"><br>
-<input type="submit" value="Login">
+<div class="form-group">
+<input type="text" name="login" placeholder="login" class="form-control"><br>
+<div>
+<div class="form-group">
+<input type="password" name="password" placeholder="password" class="form-control"><br>
+<div>
+<div class="form-group">
+<input type="submit" value="Login" class="feedBackButton btn btn-primary">
+<div>
 </form>
+<div>
--- templates/FeedBack/Form.php
+++ templates/FeedBack/Form.php
@@ -1,19 +1,28 @@
 <div class="formPosition">
 <h1>FeedBack</h1>
 <form action="<?= $formPath ?>" method="post" class="feedBackForm">
-    <input type="text" name="nick" placeholder="Enter your name"><br>
-    <textarea name="message" cols="30" rows="10">Enter your message</textarea><br>
-
-    <select name="mark">
+<div class="form-group">
+    <input type="text" name="nick" placeholder="Enter your name" class="form-control"><br>
+    </div>
+    <div class="form-group">
+    <textarea name="message" cols="30" rows="10" class="form-control">Enter your message</textarea><br>
+    </div>
+    <div class="form-group">
+    <select name="mark" class="form-control">
         <option value=”1”>1</option>
         <option value=”2”>2</option>
         <option value=”3”>3</option>
         <option value=”4”>4</option>
         <option value=”5”>5</option>
     </select>
-    <input type="submit" value="send" class="feedBackButton">
+    </div>
+    <div class="form-group">
+    <input type="submit" value="send" class="feedBackButton btn btn-primary">
+    </div>
+    
+
 </form>
 </div>
 <div class='login'>
-    <a href='/login'>Login</a>
+    <a href='/login' class="btn btn-primary btn-sm">Login</a>
 </div>
